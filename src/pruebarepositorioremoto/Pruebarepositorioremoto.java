8a7f723e456c3b68276eecdb402a5ad65e9ddf43
Sahara <Sahara@192.168.0.108>
2020-11-11 19:40:37 -0600

    Primer commit para repositorio remoto -

---
+++ src/pruebarepositorioremoto/Pruebarepositorioremoto.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebarepositorioremoto;
+
+/**
+ *
+ * @author Sahara
+ */
+public class Pruebarepositorioremoto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        system.out.println ("vamos a guardar ");
+    }
+    
+}
